3df19fb0871c0ef4acd64bfad5ffe8fac0fed54f
BUKENYA HUZAIFA <67518131+huzaifa137@users.noreply.github.com>
2020-12-05 00:31:54 +0530

    Update game.java
    Overroll modifie game

--- game.java
+++ game.java
@@ -2,6 +2,7 @@ package excellent;
 
 //A Game to pick the correct number from 0-20
 //The packages need in the program
+
 import java.util.Random;
 import java.util.Scanner;
 
@@ -13,24 +14,24 @@ public class Proj2i {
 		Scanner scan = new Scanner(System.in);
 		
 		
-		//Ask for user's name
-		System.out.println("Enter your name :");
-		String nm=scan.nextLine();
-		
 		int count=0;
+		char t1='y';
 		
 		//We're initialing and finding the random number here.
 		int x = new Random().nextInt(20);
 		
-		char t1='y';
+		//Ask for user's name
+		System.out.println("Enter your name :");
+		String nm=scan.nextLine();
+		
+		
 		
 	
-		System.out.println("\t \t \t \t \t \n" + " Welcome "+ nm);
+		System.out.println("\t \t \t \t \t \n" + " Your welcome " + "," + nm);
 
-		System.out.println("\nDo you want to play a game ?");
-		
 		//Enter user's choice to play the game or not .
-		System.out.println("\nPress y or n to continue :-");
+		System.out.println("\nDo you want to play a game ?");
+		System.out.println("\nPress y to continue OR  n  to cancel :");
 		
 		char input=scan.next().charAt(0);
 					
@@ -45,10 +46,11 @@ public class Proj2i {
 		do
 		{
 			
-			System.out.println("\nEnter your lucky number ");
-			
 			//User enter's the number of his choice between 0-20
+			System.out.println("\nEnter your lucky number ");
 			int m=scan.nextInt();
+			
+			//The Logic for the game.
 			if(m==x)
 			{
 				System.out.println("Your the winner , the answer is " + x);
@@ -57,24 +59,24 @@ public class Proj2i {
 			
 			else if(m<x)
 			{
-			System.out.println(" The number is else is less than the correct answer, try again"); 
+			System.out.println("Entered number is less than the true answer, try again"); 
 
 			}
 			
 			else if(m>x)
 			{
-				System.out.println("The number is greater than the correct answer try again");
+				System.out.println("Entered number is greater than the try try again");
 				
 			}
 			
 			else
 			{
-			System.out.println("its the 5th time playing, Game Over");
+			System.out.println("Its the 5th time, number of chances are Over");
 			}
 		count++;
 		}
 			while(count!=5);
-		System.out.println("Game Over");
+		System.out.println("\n Game Over");
 		}
 		scan.close();
 	}
